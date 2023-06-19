11814820fefc01b747f2b142cefb9cf6653e151f
vigimella <g.ciaramella98@gmail.com>
2023-06-19 15:28:10 +0200

    Multiple execution.

--- main.py
+++ main.py
@@ -3,40 +3,45 @@ import argparse
 
 from utils.extractor import AME
 
+
 def parse_args():
+    parser = argparse.ArgumentParser(prog='AME', description='Activation Maps Extractor')
+    group = parser.add_argument_group('Arguments')
 
-	parser = argparse.ArgumentParser(prog='AME', description='Activation Maps Extractor')
-	group = parser.add_argument_group('Arguments')
+    # Required parameters
+    group.add_argument('-i', '--image_size', required=True, type=int,
+                       help='Input size for test image. If model accept images in format (100, 100, 1) insert 100.')
+    group.add_argument('-c', '--channels', required=True, type=int,
+                       help='Channels for test image. If model accept images in format (100, 100, 1) insert 1.')
+    group.add_argument('-m', '--model_path', required=True, type=str, help='Path to model.')
+    group.add_argument('-p', '--image_path', required=True, type=str, help='Path to test image.')
+    group.add_argument('-o', '--output_path', required=True, type=str, help='Path for save output.')
+    # Optional parameters
+    group.add_argument('-e', '--entire_dataset', required=False, type=bool, default=False, help='Perform the extraction'
+                                                                                            'on all of the images '
+                                                                                            'contained inside the '
+                                                                                            'dataset')
+    group.add_argument('-n', '--normalize', required=False, type=bool, default=False, help='Normalize image')
 
-	# Required parameters
-	group.add_argument('-i', '--image_size', required=True, type=int, help='Input size for test image. If model accept images in format (100, 100, 1) insert 100.')
-	group.add_argument('-c', '--channels', required=True, type=int, help='Channels for test image. If model accept images in format (100, 100, 1) insert 1.')
-	group.add_argument('-m', '--model_path', required=True, type=str, help='Path to model.')
-	group.add_argument('-p', '--image_path', required=True, type=str, help='Path to test image.')
-	group.add_argument('-o', '--output_path', required=True, type=str, help='Path for save output.')
+    arguments = parser.parse_args()
 
-	# Optional parameters
-	group.add_argument('-n', '--normalize', required=False, type=bool, default=False, help='Normalize image')
-
-	arguments = parser.parse_args()
-
-	return arguments
+    return arguments
 
 
 if __name__ == '__main__':
+    arguments = parse_args()
 
-	arguments = parse_args()
+    extractor = AME(
+        arguments.image_size,
+        arguments.channels,
+        arguments.model_path,
+        arguments.image_path,
+        arguments.output_path,
+        arguments.entire_dataset,
+        arguments.normalize)
 
-	extractor = AME(
-		arguments.image_size,
-		arguments.channels,
-		arguments.model_path,
-		arguments.image_path,
-		arguments.output_path,
-		arguments.normalize)
+    print('\n[INFO] Start extraction...')
 
-	print('\n[INFO] Start extraction...')
+    extractor.extract()
 
-	extractor.extract()
-
-	print('[INFO] Extraction finished.\n')
+    print('[INFO] Extraction finished.\n')
--- utils/extractor.py
+++ utils/extractor.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorflow as tf
@@ -9,95 +10,101 @@ import cv2
 from tensorflow.keras import models
 from tensorflow.keras import layers
 
+def process_image(img_path, img_size, channels):
+    image_array = cv2.imread(img_path)
+    image_array = cv2.resize(image_array, (img_size, img_size))
+
+    if channels == 1:
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
+
+    elif channels == 3:
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+
+    image_array = np.expand_dims(image_array, axis=0)
+    image_array = image_array.reshape(-1, img_size, img_size, channels)
+
+    return image_array
+
 class AME:
 
-	def __init__(self, image_size, channels, model_path, image_path, saving_path, normalize=False):
+    def __init__(self, image_size, channels, model_path, image_path, saving_path, entire_dataset=False,
+                 normalize=False):
 
-		self.image_size = image_size
-		self.channels = channels
-		self.model_path = model_path
-		self.image_path = image_path
-		self.saving_path = saving_path
-		self.normalize = normalize
+        self.image_size = image_size
+        self.channels = channels
+        self.model_path = model_path
+        self.image_path = image_path
+        self.saving_path = saving_path
+        self.entire_dataset = entire_dataset
+        self.normalize = normalize
 
+    def manage_image(self):
 
-	def process_image(self):
+        if self.entire_dataset:
 
-		image_array = cv2.imread(self.image_path)
-		image_array = cv2.resize(image_array, (self.image_size, self.image_size))
+            for img in os.listdir(self.image_path):
+                img_path = os.path.join(self.image_path, img)
+                print(img_path)
+                process_image(img_path, self.image_size, self.channels)
+        else:
+            process_image(self.image_path, self.image_size, self.channels)
 
-		if self.channels == 1:
-			image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
+    def normalize_input(self, image_array):
 
-		elif self.channels == 3:
-			image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+        normalized_input = image_array.astype('float32') / 255
 
-		image_array = np.expand_dims(image_array, axis=0)
-		image_array = image_array.reshape(-1, self.image_size, self.image_size, self.channels)
+        return normalized_input
 
-		return image_array
+    def extract(self):
 
+        model = models.load_model(self.model_path)
 
-	def normalize_input(self, image_array):
+        # Retrieving convolutional layers name
+        conv_layers = list()
+        for layer in model.layers:
+            if 'conv' in layer.name:
+                conv_layers.append(layer.name)
 
-		normalized_input = image_array.astype('float32') / 255
+        # Processing input image
+        input_image = self.manage_image()
+        if self.normalize:
+            input_image = self.normalize_input(input_image)
 
-		return normalized_input
+        # Create saving path
+        save_path = os.path.join('output', self.saving_path)
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
 
+        # Extract feature maps for every convolutional layers
+        images_per_row = 8
 
-	def extract(self):
+        for layer in conv_layers:
 
-		model = models.load_model(self.model_path)
+            layer_output = model.get_layer(layer).output
+            activation_model = models.Model(inputs=model.input, outputs=layer_output)
+            activation = activation_model(input_image)
 
-		# Retrieving convolutional layers name
-		conv_layers = list()
-		for layer in model.layers:
-			if 'conv' in layer.name:
-				conv_layers.append(layer.name)
+            n_features = activation.shape[-1]
+            n_cols = n_features // images_per_row
+            size = activation.shape[1]
+            display_grid = np.zeros((size * n_cols, size * images_per_row))
 
+            for col in range(n_cols):
+                for row in range(images_per_row):
+                    channel_image = activation[0, :, :, col * images_per_row + row].numpy()
+                    channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+                    display_grid[col * size: (col + 1) * size, row * size: (row + 1) * size] = channel_image
 
-		# Processing input image
-		input_image = self.process_image()
-		if self.normalize:
-			input_image = self.normalize_input(input_image)
+            scale = 1. / size
 
-		# Create saving path
-		save_path = os.path.join('output', self.saving_path)
-		if not os.path.exists(save_path):
-			os.makedirs(save_path)
+            plt.figure(figsize=(2 * scale * display_grid.shape[1], 2 * scale * display_grid.shape[0]))
 
+            plt.yticks(np.arange(0, size * n_cols, size))
+            plt.xticks(np.arange(0, size * images_per_row, size))
+            plt.grid(True)
+            plt.axis('on')
+            plt.imshow(display_grid, aspect='auto', cmap='viridis')
 
-		# Extract feature maps for every convolutional layers
-		images_per_row = 8
-
-		for layer in conv_layers:
-
-			layer_output = model.get_layer(layer).output
-			activation_model = models.Model(inputs=model.input, outputs=layer_output)
-			activation = activation_model(input_image)
-
-			n_features = activation.shape[-1]
-			n_cols = n_features // images_per_row
-			size = activation.shape[1]
-			display_grid = np.zeros((size * n_cols, size * images_per_row))
-
-			for col in range(n_cols):
-				for row in range(images_per_row):
-
-					channel_image = activation[0, :, :, col*images_per_row + row].numpy()
-					channel_image = np.clip(channel_image, 0, 255).astype('uint8')
-					display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
-
-			scale = 1. / size
-
-			plt.figure(figsize=(2 * scale * display_grid.shape[1], 2 * scale * display_grid.shape[0]))
-
-			plt.yticks(np.arange(0, size*n_cols, size))
-			plt.xticks(np.arange(0, size*images_per_row, size))
-			plt.grid(True)
-			plt.axis('on')
-			plt.imshow(display_grid, aspect='auto', cmap='viridis')
-
-			savename = layer + '.png'
-			image_savepath = os.path.join(save_path, savename)
-			plt.savefig(image_savepath)
+            savename = layer + '.png'
+            image_savepath = os.path.join(save_path, savename)
+            plt.savefig(image_savepath)
